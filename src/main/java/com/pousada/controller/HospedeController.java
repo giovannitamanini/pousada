d3fc65ac9ae41367a361c7f80afa8767f727e52a
Giovanni Tamanini <giovanni.tnini@gmail.com>
2023-11-22 10:54:49 -0300

    Adicionando primeira consulta personalizada para Hospede

--- src/main/java/com/pousada/controller/HospedeController.java
+++ src/main/java/com/pousada/controller/HospedeController.java
@@ -1,10 +1,13 @@
 package com.pousada.controller;
 
+import com.pousada.domain.repository.HospedeRepository;
 import com.pousada.dto.HospedeDTO;
 import com.pousada.service.HospedeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -23,6 +26,18 @@ public class HospedeController {
     @ResponseStatus(HttpStatus.OK)
     public HospedeDTO buscarHospedePorId(@PathVariable int id) {
         return hospedeService.buscarHospedePorId(id);
+    }
+
+    @GetMapping("/hospedes/nomes/{nome}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<HospedeDTO> buscarHospedesPorNome(@PathVariable String nome) {
+        return hospedeService.buscarHospedesPorNome(nome);
+    }
+
+    @GetMapping("/hospedes")
+    @ResponseStatus(HttpStatus.OK)
+    public List<HospedeDTO> buscarTodosHospedes() {
+        return hospedeService.buscarTodosHospedes();
     }
 
     @DeleteMapping("/hospedes/{id}")
--- src/main/java/com/pousada/service/HospedeService.java
+++ src/main/java/com/pousada/service/HospedeService.java
@@ -7,6 +7,9 @@ import com.pousada.exception.HospedeNaoEncontradoException;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class HospedeService {
 
@@ -29,6 +32,32 @@ public class HospedeService {
                 .orElseThrow(() -> new HospedeNaoEncontradoException("O hóspede com o ID " + id + " não existe."));
 
         return modelMapper.map(hospedeEntity, HospedeDTO.class);
+    }
+
+    public List<HospedeDTO> buscarHospedesPorNome(String nome) {
+        List<HospedeEntity> hospedeEntities = hospedeRepository.buscarHospedesPorNome(nome);
+
+        if (hospedeEntities.isEmpty())
+            throw new HospedeNaoEncontradoException("Nenhum hóspede com o nome " + nome + " está registrado!");
+
+        List<HospedeDTO> hospedeDTOs = hospedeEntities.stream()
+                .map(hospedeEntity -> modelMapper.map(hospedeEntity, HospedeDTO.class))
+                .collect(Collectors.toList());
+
+        return hospedeDTOs;
+    }
+
+    public List<HospedeDTO> buscarTodosHospedes() {
+        List<HospedeEntity> hospedeEntities = hospedeRepository.findAll();
+
+        if (hospedeEntities.isEmpty())
+            throw new HospedeNaoEncontradoException("Nenhum hóspede está registrado!");
+
+        List<HospedeDTO> hospedeDTOs = hospedeEntities.stream()
+                .map(hospedeEntity -> modelMapper.map(hospedeEntity, HospedeDTO.class))
+                .collect(Collectors.toList());
+
+        return hospedeDTOs;
     }
 
     public void deletarHospedePorId(int id) {
