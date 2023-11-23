4869a26fca9c245e41504be7b155724a616e8bee
Giovanni Tamanini <giovanni.tnini@gmail.com>
2023-11-23 13:17:00 -0300

    Adicionando script de teste para pesquisa no banco

--- src/main/resources/selects_pousada.sql
+++ src/main/resources/selects_pousada.sql
@@ -7,3 +7,14 @@ SELECT * FROM reserva;
 SELECT * FROM estadia;
 
 SELECT * FROM hospede WHERE nome = 'Teste';
+
+SELECT * FROM reserva WHERE id_acomodacao = 4 AND (
+	-- Nova reserva totalmente dentro do período já marcado
+    "2023-11-06" >= check_in AND "2023-11-07" <= check_out
+    -- Nova reserva totalmente fora, abrangendo o período já marcado
+    OR "2023-11-06" <= check_in AND "2023-11-07" >= check_out
+    -- Nova reserva com checkIn dentro do período já marcado
+    OR "2023-11-06" > check_in AND "2023-11-06" < check_out
+    -- Nova reserva com checkOut dentro do período já marcado
+    OR "2023-11-07" > check_in AND "2023-11-07" < check_out
+);
