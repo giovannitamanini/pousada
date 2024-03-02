9b5f4be1232651c0da89b9acb39e341e22215310
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-03-02 16:05:01 -0300

    Working with the HospedeEstadia entity mapping that has a composite key. In the future, you can customize ModelMapper to better understand the code

--- src/main/java/com/pousada/service/HospedeEstadiaService.java
+++ src/main/java/com/pousada/service/HospedeEstadiaService.java
@@ -1,2 +1,41 @@
-package com.pousada.service;public class HospedeEstadiaService {
+package com.pousada.service;
+
+import com.pousada.domain.entity.HospedeEstadiaEntity;
+import com.pousada.domain.entity.HospedeEstadiaId;
+import com.pousada.domain.repository.HospedeEstadiaRepository;
+import com.pousada.dto.HospedeEstadiaDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HospedeEstadiaService {
+
+    private final ModelMapper modelMapper;
+    private final HospedeEstadiaRepository hospedeEstadiaRepository;
+
+    public HospedeEstadiaService(ModelMapper modelMapper, HospedeEstadiaRepository hospedeEstadiaRepository) {
+        this.modelMapper = modelMapper;
+        this.hospedeEstadiaRepository = hospedeEstadiaRepository;
+    }
+
+    public HospedeEstadiaDTO salvarHospedeAcompanhante(HospedeEstadiaDTO hospedeEstadiaDTO) {
+
+        HospedeEstadiaEntity hospedeEstadiaEntity = new HospedeEstadiaEntity();
+        HospedeEstadiaId hospedeEstadiaId = new HospedeEstadiaId();
+        System.out.println("HospedeEstadiaDTO = " + hospedeEstadiaDTO);
+
+
+        hospedeEstadiaId.setIdHospede(hospedeEstadiaDTO.getIdHospede());
+        hospedeEstadiaId.setIdEstadia(hospedeEstadiaDTO.getIdEstadia());
+
+        hospedeEstadiaEntity.setHospedeEstadiaId(hospedeEstadiaId);
+
+        HospedeEstadiaEntity hospedeEstadiaEntitySalva = hospedeEstadiaRepository.save(hospedeEstadiaEntity);
+
+        HospedeEstadiaDTO hospedeEstadiaDTOSalvo = new HospedeEstadiaDTO();
+        hospedeEstadiaDTOSalvo.setIdHospede(hospedeEstadiaEntitySalva.getHospedeEstadiaId().getIdHospede());
+        hospedeEstadiaDTOSalvo.setIdEstadia(hospedeEstadiaEntitySalva.getHospedeEstadiaId().getIdEstadia());
+
+        return hospedeEstadiaDTOSalvo;
+    }
 }
