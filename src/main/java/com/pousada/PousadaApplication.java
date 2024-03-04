3e0e83c113fa6023c1447aa1ff769e4fd4f9f5ce
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-03-04 07:52:42 -0300

    Adding annotation @OpenAPIDefinition in PousadaApplication main class

--- src/main/java/com/pousada/PousadaApplication.java
+++ src/main/java/com/pousada/PousadaApplication.java
@@ -1,9 +1,12 @@
 package com.pousada;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Pousada", version = "1", description = "Aplicação back-end para o sistema de gerenciamento de uma pousada"))
 public class PousadaApplication {
 
 	public static void main(String[] args) {
