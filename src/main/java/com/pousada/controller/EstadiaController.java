223487c99c8818da28126735fb9ea694fd9a67d6
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-03-04 08:25:50 -0300

    Adding Swagger documentation for EstadiaController class

--- src/main/java/com/pousada/controller/EstadiaController.java
+++ src/main/java/com/pousada/controller/EstadiaController.java
@@ -3,7 +3,10 @@ package com.pousada.controller;
 import com.pousada.dto.EstadiaDTO;
 import com.pousada.dto.ReservaDTO;
 import com.pousada.service.EstadiaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,28 +15,34 @@ import java.util.List;
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
+@Tag(name = "Estadias")
 public class EstadiaController {
 
+    @Autowired
     private final EstadiaService estadiaService;
 
+    @Operation(summary = "Inicia uma estadia a partir de uma reserva em espera")
     @PostMapping("/estadias")
     @ResponseStatus(HttpStatus.CREATED)
     public EstadiaDTO iniciarEstadia(@RequestBody ReservaDTO reserva) {
         return estadiaService.iniciarEstadia(reserva);
     }
 
+    @Operation(summary = "Busca todas as estadias cadastradas")
     @GetMapping("/estadias")
     @ResponseStatus(HttpStatus.OK)
     public List<EstadiaDTO> buscarTodasEstadias() {
         return estadiaService.buscarTodasEstadias();
     }
 
+    @Operation(summary = "Finaliza uma estadia a partir do seu Id")
     @PutMapping("/estadias/{id}")
     @ResponseStatus(HttpStatus.OK)
     public EstadiaDTO finalizarEstadia(@PathVariable int id) {
         return estadiaService.finalizarEstadia(id);
     }
 
+    @Operation(summary = "Busca todas as estadias em andamento na pousada")
     @GetMapping("/estadias/em_andamento")
     @ResponseStatus(HttpStatus.OK)
     public List<EstadiaDTO> buscarEstadiasEmAndamento() {
