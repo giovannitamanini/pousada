dcd1e7836047a015e8b921c1a4c607d129b8dc80
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-03-06 21:28:05 -0300

    Class that allows the use of the correct SQL scripts for the test profile

---
+++ src/main/java/com/pousada/configuration/TestDataSourceInitializer.java
@@ -0,0 +1,37 @@
+package com.pousada.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+@Configuration
+@Profile("test")
+public class TestDataSourceInitializer {
+
+    @Value("classpath:schema-test.sql")
+    private Resource schemaScript;
+
+    @Value("classpath:data-test.sql")
+    private Resource dataScript;
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private ResourceDatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(schemaScript);
+        populator.addScript(dataScript);
+        return populator;
+    }
+}
